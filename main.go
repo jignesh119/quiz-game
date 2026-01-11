a4d83140bf5f1f158c61e4cc3cc51d757f128e6e
revanth-motive <revanth.perumalla@gomotive.com>
2026-01-11 17:37:57 +0530

    prompting users for answers, showing score

--- main.go
+++ main.go
@@ -1,24 +1,62 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func exit(msg string){
+func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
 }
 
-func main(){
-	csvFileName:=flag.String("csv","quiz.csv","a csv file in the format of 'problem,solution'")
-	_=csvFileName
+type problem struct {
+	q string
+	a string
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+
+	return ret
+}
+
+func main() {
+	csvFileName := flag.String("csv", "quiz.csv", "a csv file in the format of 'problem,solution'")
+	_ = csvFileName
 	flag.Parse()
 
-	file,err:=os.Open(*csvFileName)
-	if err!=nil{
-		exit(fmt.Sprintf("Failed to open file %v ",*csvFileName))
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open file %v ", *csvFileName))
 	}
-	_=file
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse CSV File")
+	}
+
+	problems := parseLines(lines)
+	// fmt.Println(problems)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d %s ", i+1, p.q)
+		var ans string
+		fmt.Scanf("%s\n", &ans)
+		if ans == p.a {
+			correct++
+		}
+	}
+	fmt.Printf("You've got %d out of %d", correct, len(problems))
 }
